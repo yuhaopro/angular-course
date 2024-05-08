afbdef1d85f7a09dc0205441c156b4cc515776dd
yuhaopro <yuhao9583@gmail.com>
2024-05-08 14:49:40 +0800

    feat: assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WarningAlertComponent } from './assignment-1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './assignment-1/success-alert/success-alert.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WarningAlertComponent, SuccessAlertComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-assignments';
