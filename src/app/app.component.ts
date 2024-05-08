05ef69ebed3f1fc28580c3386e2a6a2729c19952
yuhaopro <yuhao9583@gmail.com>
2024-05-08 15:24:42 +0800

    feat: assignment-2 databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WarningAlertComponent } from './assignment-1/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './assignment-1/success-alert/success-alert.component';
-
+import { DatabindingComponent } from './assignment-2/databinding/databinding.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WarningAlertComponent, SuccessAlertComponent],
+  imports: [RouterOutlet, WarningAlertComponent, SuccessAlertComponent, DatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/assignment-2/databinding/databinding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-databinding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent{
+  username: string = '';
+
+  resetInput(): void {
+    this.username = '';
+  }
+
+}
