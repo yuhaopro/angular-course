2a908862a9739b1661cbb69c35b2c55d6d809aa7
yuhaopro <yuhao9583@gmail.com>
2024-05-11 20:32:25 +0800

    feat: assignment-6 template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { WarningAlertComponent } from './assignment-1/warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './assignment-1/success-alert/success-alert.component';
+
+/**
+ * Assignment 6 Form Guideline
+ *
+ * @description
+ * display warning message if the form is invalid and touched
+ * display a warning message below each input if invalid
+ * Upon submitting, print the value of the form to the console
+ */
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WarningAlertComponent, SuccessAlertComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-assignments';
+export class AppComponent implements OnInit {
+  @ViewChild('form') subscriptionForm: NgForm | undefined;
+  ngOnInit(): void {}
+  title: string = 'angular-assignments';
+  defaultSubscription = 'Basic';
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  onSubmit() {
+    console.log(this.subscriptionForm?.value.email);
+    console.log(this.subscriptionForm?.value.subscription);
+    console.log(this.subscriptionForm?.value.password);
+  }
 }
